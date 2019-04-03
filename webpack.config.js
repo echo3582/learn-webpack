1be3894d5d9b0c33d3146d1551f600fa513c5679
echo3582 <qianlu3582@163.com>
2019-04-03 16:25:33 +0800

    【功能】 配置webpack
    - 安装自动生成HTML插件 html-webpack-plugin
    - 编写webpack配置文件

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+//定义了一些文件夹的路径
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+  entry: APP_PATH,
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Hello World app'
+    })
+  ]
+}
